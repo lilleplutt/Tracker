f1f5d1d15c339d57c25d68e7ed1159b2681cac49
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-11-12 17:27:28 +0300

    add files and classes for tabBar-, tracker-, statistics- controllers

---
+++ Tracker/StatisticsController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+final class StatisticsController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+}
---
+++ Tracker/TabBarController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+final class TabBarController: UITabBarController {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
