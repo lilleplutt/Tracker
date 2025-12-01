d299d07831603b501cb4d606be89d29acc640359
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-12-01 11:33:21 +0300

    create TrackersCollectionViewCell

---
+++ Tracker/TrackersList/TrackersCollectionViewCell.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+final class TrackersCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
