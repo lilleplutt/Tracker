295539f55fb48c77e074d8dff3453dfab5c1feae
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-12-01 16:13:44 +0300

    add button, navBar, constraints in scheduleCont

--- Tracker/TrackersList/ScheduleViewController.swift
+++ Tracker/TrackersList/ScheduleViewController.swift
@@ -2,5 +2,42 @@ import UIKit
 
 final class ScheduleViewController: UIViewController {
     
+    //MARK: - UI elements
+    private lazy var readyButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Готово", for: .normal)
+        button.setTitleColor(.ypWhiteIOS, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
+        button.backgroundColor = .ypBlackIOS
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 16
+        return button
+    }()
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpView()
+        setUpConstraints()
+    }
+    
+    //MARK: - Private methods
+    private func setUpView() {
+        view.backgroundColor = .ypWhiteIOS
+        navigationItem.title = "Расписание"
+        navigationController?.navigationBar.titleTextAttributes = [
+            .foregroundColor: UIColor.ypBlackIOS,
+            .font: UIFont.systemFont(ofSize: 16, weight: .medium)
+        ]
+    }
+    
+    private func setUpConstraints() {
+        NSLayoutConstraint.activate([
+            readyButton.heightAnchor.constraint(equalToConstant: 60),
+            readyButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            readyButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16)
+        ])
+    }
 }
 
