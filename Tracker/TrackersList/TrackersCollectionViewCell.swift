11e12d41b31ba0080e231bfdd7ca74d2a9d403c3
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-12-04 12:36:24 +0300

    add ui elements for trackerCell

--- Tracker/TrackersList/TrackersCollectionViewCell.swift
+++ Tracker/TrackersList/TrackersCollectionViewCell.swift
@@ -2,6 +2,60 @@ import UIKit
 
 final class TrackersCollectionViewCell: UICollectionViewCell {
     
+    //MARK: - UI elements
+    private let colorView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 16
+        view.layer.masksToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let emojiLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16, weight: .medium)
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        label.textColor = .ypWhiteIOS
+        label.numberOfLines = 2
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let daysCountLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        label.textColor = .ypBlackIOS
+        label.text = "0 дней"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var plusButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("+", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        button.tintColor = .white
+        button.backgroundColor = .ypBlackIOS
+        button.layer.cornerRadius = 17
+        button.layer.masksToBounds = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        button.addTarget(self, action: #selector(plusButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    //MARK: - Properties
+    private var trackerId: UUID?
+    private var isCompleted = false
+    
+    // MARK: - Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
@@ -11,11 +65,23 @@ final class TrackersCollectionViewCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Private methods
     private func setupUI() {
+        contentView.addSubview(colorView)
+        colorView.addSubview(emojiLabel)
+        colorView.addSubview(titleLabel)
+        contentView.addSubview(daysCountLabel)
+        contentView.addSubview(plusButton)
         
-        backgroundColor = .ypBlueIOS
         layer.cornerRadius = 16
         layer.masksToBounds = true
+    }
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            
+            
+            
+        ])
     }
     
     func configure(with tracker: Tracker) {
