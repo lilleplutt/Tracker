aa0e9b809e022afe0539b87c7290869262c2d4a2
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-12-04 12:13:49 +0300

    add new funcs in trackerCell

--- Tracker/TrackersList/TrackersCollectionViewCell.swift
+++ Tracker/TrackersList/TrackersCollectionViewCell.swift
@@ -4,11 +4,22 @@ final class TrackersCollectionViewCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+        setupUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        
+        backgroundColor = .ypBlueIOS
+        layer.cornerRadius = 16
+        layer.masksToBounds = true
+    }
+    
+    func configure(with tracker: Tracker) {
+        backgroundColor = tracker.color
     }
 }
 
