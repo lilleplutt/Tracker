980bb9f9203d5e8d07dd2484989a7153e9a71f04
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-12-18 16:59:26 +0300

    add pages for onboarding

--- Tracker/Onboarding/OnboardingViewController.swift
+++ Tracker/Onboarding/OnboardingViewController.swift
@@ -2,4 +2,21 @@ import UIKit
 
 final class OnboardingViewController: UIPageViewController {
     
+    lazy var pages: [UIViewController] = {
+            let pageOne = UIViewController()
+            pageOne.view.backgroundColor = .red
+            
+            let pageTwo = UIViewController()
+            pageTwo.view.backgroundColor = .green
+            
+            return [pageOne, pageTwo]
+        }()
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            if let first = pages.first {
+                setViewControllers([first], direction: .forward, animated: true, completion: nil)
+            }
+        }
 }
