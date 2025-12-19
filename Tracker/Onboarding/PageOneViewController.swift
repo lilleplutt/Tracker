08c696f10526c48137272a230de7907fb11b136b
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-12-19 16:55:49 +0300

    add ui for pageone

--- Tracker/Onboarding/PageOneViewController.swift
+++ Tracker/Onboarding/PageOneViewController.swift
@@ -2,4 +2,52 @@ import UIKit
 
 final class PageOneViewController: UIViewController {
     
+    //MARK: - UI Elements
+    private lazy var onboardingImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(resource: .onboardingOne))
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var onboardingLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Отслеживайте только то, что хотите"
+        label.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+        label.textColor = UIColor(resource: .ypBlackIOS)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var onboardingButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Вот это технологии!", for: .normal)
+        button.setTitleColor(.ypWhiteIOS, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        button.backgroundColor = .ypBlackIOS
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 16
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setupConstraints()
+    }
+    
+    //MARK: - Private Methods
+    private func setupUI() {
+        view.addSubview(onboardingImageView)
+        view.addSubview(onboardingLabel)
+        view.addSubview(onboardingButton)
+    }
+    
+    private func setupConstraints() {
+        
+    }
+    
 }
