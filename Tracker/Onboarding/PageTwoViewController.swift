f9bc40588012890a88660298b765b0f96e9bc25a
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-12-19 17:15:00 +0300

    add pageTwo

--- Tracker/Onboarding/PageTwoViewController.swift
+++ Tracker/Onboarding/PageTwoViewController.swift
@@ -2,4 +2,68 @@ import UIKit
 
 final class PageTwoViewController: UIViewController {
     
+    //MARK: - UI Elements
+    private lazy var onboardingImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(resource: .onboardingTwo))
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var onboardingLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Даже если это не литры воды и йога"
+        label.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+        label.textColor = UIColor(resource: .ypBlackIOS)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var onboardingButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Вот это технологии!", for: .normal)
+        button.setTitleColor(.ypWhiteIOS, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        button.backgroundColor = .ypBlackIOS
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 16
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setupConstraints()
+    }
+    
+    //MARK: - Private Methods
+    private func setupUI() {
+        view.addSubview(onboardingImageView)
+        view.addSubview(onboardingLabel)
+        view.addSubview(onboardingButton)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            onboardingImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            onboardingImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            onboardingImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            onboardingImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            onboardingLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            onboardingLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            onboardingLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            onboardingLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            onboardingButton.topAnchor.constraint(equalTo: onboardingLabel.bottomAnchor, constant: 160),
+            onboardingButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            onboardingButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            onboardingButton.heightAnchor.constraint(equalToConstant: 60)
+            ])
+    }
+    
 }
+
