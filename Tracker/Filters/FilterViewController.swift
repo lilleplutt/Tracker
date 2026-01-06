8c1fd048cdab68ad7511d6863a2f839ca4d322a9
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2026-01-06 14:52:32 +0300

    add ui elements and constraints for filterVC

--- Tracker/Filters/FilterViewController.swift
+++ Tracker/Filters/FilterViewController.swift
@@ -2,5 +2,48 @@ import UIKit
 
 final class FilterViewController: UIViewController {
     
+    //MARK: - UI Elements
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.backgroundColor = .ypWhiteIOS
+        tableView.register(FilterCell.self, forCellReuseIdentifier: "FilterCell")
+        tableView.separatorStyle = .none
+        tableView.rowHeight = 75
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    //MARK: - Private Methods
+    private func setupUI() {
+        view.backgroundColor = .ypWhiteIOS
+        
+        navigationItem.title = NSLocalizedString("filter.title", comment: "Filter screen title")
+        navigationController?.navigationBar.titleTextAttributes = [
+            .foregroundColor: UIColor.ypBlackIOS,
+            .font: UIFont.systemFont(ofSize: 16, weight: .medium)
+        ]
+        navigationItem.hidesBackButton = true
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        view.addSubview(tableView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
+    }
 }
+
+//MARK: - Extensions
 
